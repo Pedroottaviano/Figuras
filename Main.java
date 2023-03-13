1644715b88df1e25301ccdb2eaeb8f5c3dee551a
pedro-ottaviano-globant <pedro.ottaviano@globant.com>
2023-03-13 10:05:42 -0300

    segundo commit

--- Main.java
+++ Main.java
@@ -3,16 +3,16 @@ package Figuras;
 public class Main {
     public static void main(String[] args) {       
 
-        Triangulo triangulo = new Triangulo("gris", 2.5f, 5.6f);
+        Figura triangulo = new Triangulo("gris", 2.5f, 5.6f);
         System.out.println(triangulo.toString());
         System.out.println("-------------------------------");
 
-        Cuadrado cuadrado = new Cuadrado("verde", 4.2f);
+        Figura cuadrado = new Cuadrado("verde", 4.2f);
         System.out.println(cuadrado.toString());
 
         System.out.println("-------------------------------");
 
-        Circunferencia circunferencia = new Circunferencia("Amarillo", 3.5f);
+        Figura circunferencia = new Circunferencia("Amarillo", 3.5f);
         System.out.println(circunferencia.toString());
     }
 }
