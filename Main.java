55cdb8758b6d6d0137ee071f77260cee3238ab5c
pedro-ottaviano-globant <pedro.ottaviano@globant.com>
2023-03-16 09:25:19 -0300

    Agregamos titulo al programa

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package Figuras;
 
 public class Main {
     public static void main(String[] args) {       
-
+        System.out.println("----Programa que imprime figuras geometricas----");
         Figura triangulo = new Triangulo("gris", 2.5f, 5.6f);
         System.out.println(triangulo.toString());
         System.out.println("-------------------------------");
